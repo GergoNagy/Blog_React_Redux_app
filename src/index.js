1c4d63ef9af7aa9544cfd9946e7d0b1f83c015de
Gergo Nagy <guzmop@gmail.com>
2017-11-12 16:53:17 -0800

    add an api request to the post request

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom'
+import promise from 'redux-promise'
 import reducers from './reducers';
 import PostIndex from './components/postIdex'
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
