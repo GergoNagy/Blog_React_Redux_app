22fbed2c4bb35bc8b1a661e17f17fe57359fdabf
Gergo Nagy <guzmop@gmail.com>
2017-11-12 21:47:37 -0800

    list the posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
-import PostsReducer from './reducerPosts'
+import PostsReducer from './reducer_posts'
+
 const rootReducer = combineReducers({
   posts: PostsReducer
 });
