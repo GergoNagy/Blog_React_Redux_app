bf506cbdb32bdd482dfe325b6424375dcbe56c0b
Gergo Nagy <guzmop@gmail.com>
2017-11-16 21:50:54 +0000

    add error validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class PostNew extends Component {
-    render() {
+    renderField(field) {
         return (
-            <div>
-                PostNew
+            <div className='form-group'>
+                <label>{field.label}</label>
+                <input
+                    className='form-control'
+                    type='text'
+                    {...field.input} 
+                />
+                {field.meta.error}
             </div>
+        )
+    }
+
+    onSubmit(values) {
+        console.log(values)
+    }
+
+    render() {
+        const { handleSubmit } = this.props
+        
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label='Title for Post'
+                    name='title'
+                    component={this.renderField}
+                />
+                <Field
+                    label='Categories'
+                    name='categories'
+                    component={this.renderField}
+                />
+                <Field
+                    label='Post Content'
+                    name='content'
+                    component={this.renderField}
+                />
+                <button 
+                    type='submit' 
+                    className='btn btn-primary'>
+                    Submit
+                </button>
+            </form>
         )
     }
 }
 
-export default PostNew
+function validate(values) {
+    const errors = {}
+    if(!values.title) {
+        errors.title = "Enter a title!"
+    }
+    if(!values.categories) {
+        errors.categories = "Enter some categories"
+    }
+    if(!values.content) {
+        errors.content = "Enter some content"
+    }
+
+    return errors
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+}) (PostNew)
